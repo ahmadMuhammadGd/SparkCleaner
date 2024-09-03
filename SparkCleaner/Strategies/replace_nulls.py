50519ea8fa3cac90005b35c8834384e0a639277a
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-09-03 17:41:16 +0300

    fixed replace nulls bug

--- SparkCleaner/Strategies/replace_nulls.py
+++ SparkCleaner/Strategies/replace_nulls.py
@@ -1,9 +1,8 @@
 from SparkCleaner.Strategies.base import BaseCleaningStrategy, DataFrame
 
 class FillNAStrategy(BaseCleaningStrategy):
-    def __init__(self, fill_values: dict = None, columns: list = None):
-        super().__init__(columns)
+    def __init__(self, fill_values: dict = None):
         self.fill_values = fill_values if fill_values else {}
 
     def clean(self, df: DataFrame) -> DataFrame:
-        return df.fillna({self.fill_values})
+        return df.fillna(self.fill_values)
